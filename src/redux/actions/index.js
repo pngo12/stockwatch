740e95884e2ec52e0bd34d98d71f8c42c76dce09
pngo12 <pngo12@gmail.com>
2018-12-13 12:14:11 -0800

    async/await actions  & .env var

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -6,39 +6,39 @@ import {
     GET_DATA
 } from '../constants';
 
-export const getData = ticker => dispatch => {
-
-    axios.all([
-        axios.get(`https://api.iextrading.com/1.0/stock/${ticker}/quote`),
-        axios.get(`https://api.iextrading.com/1.0/stock/${ticker}/company`),
-        axios.get(`https://api.iextrading.com/1.0/stock/${ticker}/earnings`),
-        axios.get(`https://api.iextrading.com/1.0/stock/${ticker}/peers`),
-        axios.get(`https://api.iextrading.com/1.0/stock/${ticker}/chart/1d`),
-        axios.get(`https://api.iextrading.com/1.0/stock/${ticker}/news`),
-    ])
-        .then(axios.spread((quote, company, earnings, peers, chart, news) => {
-            dispatch({
-                type: RECEIVED_ALL_DATA,
-                payload: {
-                    quote: quote.data,
-                    company: company.data,
-                    earnings: earnings.data,
-                    peers: peers.data,
-                    chart: chart.data,
-                    ticker: quote.data.symbol,
-                    news: news.data,
-                }
-            })
-        }))
-        .catch(err => {
-            dispatch({ type: WRONG_SYMBOL, payload: err.response });
-        })
+export const getData = ticker => async dispatch => {
+    try {
+        const getAllData = await axios.all([
+            axios.get(process.env.REACT_APP_API + `/${ticker}/quote`),
+            axios.get(process.env.REACT_APP_API + `/${ticker}/company`),
+            axios.get(process.env.REACT_APP_API + `/${ticker}/earnings`),
+            axios.get(process.env.REACT_APP_API + `/${ticker}/peers`),
+            axios.get(process.env.REACT_APP_API + `/${ticker}/chart/1d`),
+            axios.get(process.env.REACT_APP_API + `/${ticker}/news`),
+        ])
+        const [quote, company, earnings, peers, chart, news] = getAllData
+        dispatch({
+            type: RECEIVED_ALL_DATA,
+            payload: {
+                quote: quote.data,
+                company: company.data,
+                earnings: earnings.data,
+                peers: peers.data,
+                chart: chart.data,
+                ticker: quote.data.symbol,
+                news: news.data,
+            }
+        });
+    }
+    catch (error) {
+        dispatch({ type: WRONG_SYMBOL, payload: error.response })
+    }
 }
 
 export const getChartDate = (range, ticker) => async dispatch => {
     dispatch({ type: IS_LOADING });
     try {
-        const chartData = await axios.get(`https://api.iextrading.com/1.0/stock/${ticker}/chart/${range}`);
+        const chartData = await axios.get(process.env.REACT_APP_API + `/${ticker}/chart/${range}`);
         dispatch({ type: GET_DATA, payload: chartData.data })
     }
     catch (err) {
