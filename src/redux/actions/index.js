4f1e64924a57d2f2743f447d4cf31c583ce29d08
pngo12 <pngo12@gmail.com>
2018-12-13 12:09:53 -0800

    refactored async/await

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -16,18 +16,17 @@ export const getData = ticker => async dispatch => {
             axios.get(process.env.REACT_APP_API + `/${ticker}/chart/1d`),
             axios.get(process.env.REACT_APP_API + `/${ticker}/news`),
         ])
-        console.log("FIRST ONE :", getAllData[0].data, "\nSECOND ONE: ", getAllData[1].data)
-
+        const [quote, company, earnings, peers, chart, news] = getAllData
         dispatch({
             type: RECEIVED_ALL_DATA,
             payload: {
-                quote: getAllData[0].data,
-                company: getAllData[1].data,
-                earnings: getAllData[2].data,
-                peers: getAllData[3].data,
-                chart: getAllData[4].data,
-                ticker: getAllData[5].data,
-                news: getAllData[6].data,
+                quote: quote.data,
+                company: company.data,
+                earnings: earnings.data,
+                peers: peers.data,
+                chart: chart.data,
+                ticker: quote.data.symbol,
+                news: news.data,
             }
         })
     }
@@ -35,33 +34,6 @@ export const getData = ticker => async dispatch => {
     catch (error) {
         dispatch({ type: WRONG_SYMBOL, payload: error.response })
     }
-    //     axios.all([
-    //         axios.get(process.env.REACT_APP_API + `/${ticker}/quote`),
-    //         axios.get(process.env.REACT_APP_API + `/${ticker}/company`),
-    //         axios.get(process.env.REACT_APP_API + `/${ticker}/earnings`),
-    //         axios.get(process.env.REACT_APP_API + `/${ticker}/peers`),
-    //         axios.get(process.env.REACT_APP_API + `/${ticker}/chart/1d`),
-    //         axios.get(process.env.REACT_APP_API + `/${ticker}/news`),
-    //     ])
-    //         .then(axios.spread((quote, company, earnings, peers, chart, news) => {
-    //             dispatch({
-    //                 type: RECEIVED_ALL_DATA,
-    //                 payload: {
-    //                     quote: quote.data,
-    //                     company: company.data,
-    //                     earnings: earnings.data,
-    //                     peers: peers.data,
-    //                     chart: chart.data,
-    //                     ticker: quote.data.symbol,
-    //                     news: news.data,
-    //                 }
-    //             })
-    //         }))
-    //         .catch(err => {
-    //             dispatch({ type: WRONG_SYMBOL, payload: err.response });
-    //         })
-    // }
-
 }
 
 export const getChartDate = (range, ticker) => async dispatch => {
